8f94487c6f8a577961ed020ab337175aa8223ff1
abdialimovtemur <abdialimovtemur@gmail.com>
2024-10-11 08:32:31 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt sint atque neque laboriosam, animi explicabo perferendis distinctio voluptates ipsam eius aliquid accusamus sequi quas ea quibusdam assumenda quam facere recusandae.</h1>
+    </div>
+  );
+}
+
+export default App;
